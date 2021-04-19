2adaf37395bf4f8806688fe11c49b502d4af8b7d
Alex Cosmas <2alexcosmas@gmail.com>
2021-04-19 16:35:20 +0300

    Refined the Login Form

--- pages/index.js
+++ pages/index.js
@@ -2,55 +2,78 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className=' bg-default-light h-full w-full min-h-screen text-default-primary flex items-center justify-center text-lg'>
-      <div className='flex flex-col w-full items-center'>
-        <Image
-          src='/logo/SVG.logo.purple.dark.svg'
-          alt='Picture of the author'
-          width='200'
-          height='45'
-          className='object-contain'
-        />
-        <div className='bg-white text-default-primary mt-6 w-1/2 px-12 pt-12 pb-16 shadow-sm'>
-          <div className='flex flex-col'>
-            <div className='my-3 flex flex-col'>
-              <label htmlFor='' className='px-3 font-medium mb-2'>
-                User Name
-              </label>
-              <input
-                type='text'
-                placeholder='John Doe'
-                className=' bg-default-light px-3 py-2'
-              />
-            </div>
+    <div>
+      <div className='min-h-screen bg-default-light flex flex-col justify-center py-12 sm:px-6 lg:px-8'>
+        <div className='sm:mx-auto sm:w-full sm:max-w-md flex flex-col justify-center items-center'>
+          <Image
+            src='/logo/SVG.logo.purple.dark.svg'
+            alt='Picture of the author'
+            width='200'
+            height='45'
+            className='mx-auto w-auto object-contain'
+          />
+        </div>
 
-            <div className='my-3 flex flex-col'>
-              <label htmlFor='' className='px-3 font-medium mb-2'>
-                Password
-              </label>
-              <input
-                type='password'
-                placeholder='****************'
-                className=' bg-default-light px-3 py-2'
-              />
-            </div>
+        {/* Form  */}
 
-            <div class='flex items-center my-3'>
-              <input
-                id='passwordremember'
-                name='passwordremember'
-                type='checkbox'
-                class='focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded mr-4'
-              />
-              Remember my password
-            </div>
+        <div className='mt-8 sm:mx-auto sm:w-full sm:max-w-md'>
+          <div className='bg-white pt-8 pb-12 px-4 shadow sm:rounded-lg sm:px-10 text-default-primary'>
+            <form className='space-y-6' action='#' method='POST'>
+              <div className='my-3 flex flex-col'>
+                <label
+                  htmlFor='email'
+                  className='mb-2 block text-sm font-medium px-3'
+                >
+                  User Name
+                </label>
+                <input
+                  type='text'
+                  placeholder='John Doe'
+                  className=' bg-default-light appearance-none block w-full px-3 py-2 border border-gray-50 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-default-primary focus:border-default-primary sm:text-sm'
+                />
+              </div>
 
-            <button className='  bg-default-primary text-white py-4 px-8 rounded-md mt-5 font-semibold'>
-              LOG IN
-            </button>
+              <div className='my-3 flex flex-col'>
+                <label
+                  htmlFor='password'
+                  className='mb-2 block text-sm font-medium px-3'
+                >
+                  Password
+                </label>
+                <input
+                  type='password'
+                  placeholder='************'
+                  className=' bg-default-light appearance-none block w-full px-3 py-2 border border-gray-50 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-default-primary focus:border-default-primary sm:text-sm'
+                />
+              </div>
+
+              <div class='flex items-center'>
+                <input
+                  id='remember_me'
+                  name='remember_me'
+                  type='checkbox'
+                  className='h-4 w-4  bg-default-light rounded'
+                />
+                <label
+                  htmlFor='remember_me'
+                  className='ml-2 block text-sm font-medium'
+                >
+                  Remember my password
+                </label>
+              </div>
+
+              <button className='  w-full bg-default-primary text-white py-4 px-8 rounded-md mt-5 font-semibold focus:outline-none focus:ring-default-primary focus:border-default-primary '>
+                LOG IN
+              </button>
+            </form>
           </div>
         </div>
-        <div className='mt-8 font-medium'>Forgot your password</div>
+
+        {/* End of Form  */}
+
+        <div className='mt-8 font-medium mx-auto text-default-primary'>
+          Forgot your password
+        </div>
       </div>
     </div>
   )
